6d05f65392223eeddaf7237812219bd08da7c0fd
AkshayaRaju <achuraju28@gmail.com>
2017-07-28 21:50:15 +0530

    Create FizzBuzz.java

---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class FizzBuzz{
+public static void main(String args[]){
+  String result;
+  for(int i=1;i<=100;i++)
+  {
+    result=" ";
+    if(i%3==0)
+      result+="Fizz";
+    if(i%5==0)
+      result+="Buzz";
+    if(result==" ")
+      result+=i;
+    System.out.println(result);
+  }
+}
+    
+  
